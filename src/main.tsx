ca5cead177d03c3c15b766faab804a439fd76f20
whoamizy <delovoysergei098@gmail.com>
2023-11-07 13:18:22 +0400

    feat: add consta ui kit and toastify container

--- src/main.tsx
+++ src/main.tsx
@@ -3,15 +3,21 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { I18nextProvider } from 'react-i18next'
 import { BrowserRouter } from 'react-router-dom'
+import { ToastContainer } from 'react-toastify'
+import { Theme, presetGpnDefault } from '@consta/uikit/Theme'
 
+import 'react-toastify/dist/ReactToastify.css'
 import i18n from 'shared/i18n'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <BrowserRouter>
       <I18nextProvider i18n={i18n}>
-        <App />
+        <Theme preset={presetGpnDefault}>
+          <App />
+        </Theme>
       </I18nextProvider>
     </BrowserRouter>
+    <ToastContainer />
   </React.StrictMode>,
 )
