9733fedfbb53982182532132a7218d7ec3d4e420
whoamizy <delovoysergei098@gmail.com>
2023-11-07 21:45:53 +0400

    feat: add fonts, add global and overrides styles, add nullstyles for consta ui kit

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,23 @@
+import 'react-toastify/dist/ReactToastify.min.css'
+import 'shared/styles/globals.scss'
+import 'shared/styles/theme/_color/Theme_color_theme.scss'
+import 'shared/styles/theme/_control/Theme_control_theme.scss'
+import 'shared/styles/theme/_font/Theme_font_theme.scss'
+import 'shared/styles/theme/_size/Theme_size_theme.scss'
+import 'shared/styles/theme/_space/Theme_space_theme.scss'
+import 'shared/styles/theme/_shadow/Theme_shadow_theme.scss'
+
 import { App } from 'app/App'
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { I18nextProvider } from 'react-i18next'
 import { BrowserRouter } from 'react-router-dom'
 import { ToastContainer } from 'react-toastify'
-import { Theme, presetGpnDefault } from '@consta/uikit/Theme'
+import { Theme } from '@consta/uikit/Theme'
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
 import { AuthProvider, QueryClientProvider } from 'shared/providers'
+import { themePreset } from 'shared/styles/theme'
 
-import 'react-toastify/dist/ReactToastify.css'
 import i18n from 'shared/i18n'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
@@ -15,9 +25,10 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
     <QueryClientProvider>
       <BrowserRouter>
         <I18nextProvider i18n={i18n}>
-          <Theme preset={presetGpnDefault}>
+          <Theme preset={themePreset}>
             <AuthProvider>
               <App />
+              <ReactQueryDevtools initialIsOpen={false} position="right" />
             </AuthProvider>
           </Theme>
         </I18nextProvider>
---
+++ src/shared/styles/theme/theme.ts
@@ -0,0 +1,14 @@
+import { ThemePreset } from '@consta/uikit/Theme'
+
+export const themePreset: ThemePreset = {
+  color: {
+    primary: 'theme',
+    accent: 'theme',
+    invert: 'theme',
+  },
+  control: 'theme',
+  font: 'theme',
+  size: 'theme',
+  space: 'theme',
+  shadow: 'theme',
+}
