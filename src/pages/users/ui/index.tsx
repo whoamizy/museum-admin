de76d71cc1e10d8efc757ac335420594348f5976
whoamizy <delovoysergei098@gmail.com>
2023-11-08 23:31:23 +0400

    feat: add filter users

--- src/pages/users/ui/index.tsx
+++ src/pages/users/ui/index.tsx
@@ -1,5 +1,5 @@
 import { SearchBar } from "widgets/search-bar"
-import { useState } from "react"
+import { useCallback, useEffect, useState } from "react"
 import { useTranslation } from "react-i18next"
 import { ContentPlate } from "shared/components"
 import styles from './styles.module.scss'
@@ -12,6 +12,20 @@ export const UsersPage = () => {
   const { t } = useTranslation()
   const [searchValue, setSearchValue] = useState<string | null>("")
   const { data: users, isLoading } = useGetAllUsers()
+  const [filteredUsers, setFilteredUsers] = useState(users)
+
+  const filterVariantsHandler = useCallback(() => {
+    if (!users) return
+    if (searchValue) {
+      setFilteredUsers(users.filter(({ username }) => username.toLowerCase().startsWith(searchValue.toLowerCase())))
+    } else {
+      setFilteredUsers(users)
+    }
+  }, [searchValue, users])
+
+  useEffect(() => {
+    filterVariantsHandler()
+  }, [filterVariantsHandler])
 
   return (
     <div className={styles.wrapper}>
@@ -22,7 +36,6 @@ export const UsersPage = () => {
       />
       <ContentPlate>
         <h1 className={styles.title}>{t('users.title')}</h1>
-
         <div className={styles.topLine}>
           <div className={styles.topLineCategory}>
             {t('users.name')}
@@ -33,12 +46,12 @@ export const UsersPage = () => {
           <div className={styles.topLineCategory}></div>
         </div>
         {isLoading && <ContentLoader />}
-        {!!users &&
+        {!!filteredUsers &&
           <>
-            {users.length === 0 && <NotFound />}
+            {filteredUsers.length === 0 && <NotFound />}
             <div className={styles.users}>
               <ul>
-                {users.map((user) =>
+                {filteredUsers.map((user) =>
                   <li key={user._id}>
                     <UsersItem {...user} />
                   </li>
