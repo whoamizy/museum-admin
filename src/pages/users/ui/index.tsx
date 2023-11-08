fdfbddad2ab42be0700e50e2727b31e2be1843de
whoamizy <delovoysergei098@gmail.com>
2023-11-08 16:15:35 +0400

    feat: add content plate

--- src/pages/users/ui/index.tsx
+++ src/pages/users/ui/index.tsx
@@ -1,18 +1,23 @@
 import { SearchBar } from "widgets/search-bar"
 import { useState } from "react"
 import { useTranslation } from "react-i18next"
+import { ContentPlate } from "shared/components"
+import styles from './styles.module.scss'
 
 export const UsersPage = () => {
   const { t } = useTranslation()
   const [searchValue, setSearchValue] = useState<string | null>("")
 
   return (
-    <div>
+    <div className={styles.wrapper}>
       <SearchBar
         value={searchValue}
         setValue={setSearchValue}
         placeholder={t('users.search')}
       />
+      <ContentPlate>
+        alskujdhalkjwsdjk
+      </ContentPlate>
     </div>
   )
 }
--- src/shared/components/index.ts
+++ src/shared/components/index.ts
@@ -1,1 +1,2 @@
 export * from './sidebar-link'
+export * from './content-plate'
