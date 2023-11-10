6a638a8de43f6421f2b77d63414d17ddffbbbd3d
whoamizy <delovoysergei098@gmail.com>
2023-11-10 22:16:12 +0400

    feat: add index route

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,13 @@
 import { Route, Routes } from "react-router-dom";
 import { routes } from "./config";
+import { AppLinks } from "shared/enums";
 
 export const Router = () => {
   return (
     <Routes>
       {routes.map(({ path, element }) =>
         <Route
+          index={path === AppLinks.LOGIN}
           key={path}
           path={path}
           element={element}
