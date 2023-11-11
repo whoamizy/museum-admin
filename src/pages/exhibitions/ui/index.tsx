c20cd829b5f1532eb567b4a80c781dbf77174289
whoamizy <delovoysergei098@gmail.com>
2023-11-11 11:30:44 +0400

    feat: show exhibitions list

--- src/pages/exhibitions/ui/index.tsx
+++ src/pages/exhibitions/ui/index.tsx
@@ -1,17 +1,35 @@
-import { useState } from "react"
+import { useCallback, useEffect, useState } from "react"
 import { useTranslation } from "react-i18next"
-import { PageWrapper } from "shared/components"
+import { ContentPlate, PageWrapper } from "shared/components"
 import { SearchBar } from "widgets/search-bar"
 import styles from './styles.module.scss'
 import { Button } from "@consta/uikit/Button"
 import { useNavigate } from "react-router-dom"
 import { AppLinks } from "shared/enums"
+import { useGetAllExhibitions } from "shared/api"
+import { ContentLoader } from "widgets/content-loader"
+import { NotFound } from "widgets/not-found"
+import { ExhibitionItem } from "./exhibition-item"
 
 export const ExhibitionsPage = () => {
   const { t } = useTranslation()
   const [searchValue, setSearchValue] = useState<string | null>("")
-
+  const { data: exhibitions, isLoading } = useGetAllExhibitions()
+  const [filteredExhibitions, setFilteredExhibitions] = useState(exhibitions)
   const navigate = useNavigate()
+
+  const filterVariantsHandler = useCallback(() => {
+    if (!exhibitions) return
+    if (searchValue) {
+      setFilteredExhibitions(exhibitions.filter(({ name }) => name.toLowerCase().startsWith(searchValue.toLowerCase())))
+    } else {
+      setFilteredExhibitions(exhibitions)
+    }
+  }, [exhibitions, searchValue])
+
+  useEffect(() => {
+    filterVariantsHandler()
+  }, [filterVariantsHandler])
 
   const navigateToNew = () => {
     navigate(AppLinks.ADD_EXHIBITIONS)
@@ -43,6 +61,21 @@ export const ExhibitionsPage = () => {
           {t('exhibitions.address')}
         </div>
       </div>
+      <ContentPlate>
+        {isLoading && <ContentLoader />}
+        {!!filteredExhibitions &&
+          <>
+            {filteredExhibitions.length === 0 && <NotFound />}
+            <ul>
+              {filteredExhibitions.map((exhibition) =>
+                <li key={exhibition._id}>
+                  <ExhibitionItem {...exhibition} />
+                </li>
+              )}
+            </ul>
+          </>
+        }
+      </ContentPlate>
     </PageWrapper>
   )
 }
