1701d8a2bb422d9f7529b669999c0452fc8b179b
whoamizy <delovoysergei098@gmail.com>
2023-11-13 12:19:15 +0400

    feat: add collapse collection logic

--- src/pages/collections/ui/collections-item.tsx
+++ src/pages/collections/ui/collections-item.tsx
@@ -5,11 +5,18 @@ import { useDeleteCollection } from "shared/api"
 import { queryClient } from "shared/providers"
 import styles from './styles.module.scss'
 import cn from 'classnames'
-import { DeleteIcon, EditIcon, PlusIcon } from "shared/assets/icons"
+import { ArrowIcon, DeleteIcon, EditIcon, PlusIcon } from "shared/assets/icons"
+import { CollectionItems } from "widgets/collection-items"
+import { useState } from "react"
 
 export const CollectionsItem = ({ _id, name }: Collection) => {
   const { t } = useTranslation()
   const { mutateAsync: deleteCollection } = useDeleteCollection()
+  const [isCollapsed, setIsCollapsed] = useState(false)
+
+  const collapseCollection = () => {
+    setIsCollapsed(prev => !prev)
+  }
 
   const deleteHandler = async () => {
     await deleteCollection(_id, {
@@ -24,21 +31,31 @@ export const CollectionsItem = ({ _id, name }: Collection) => {
   }
 
   return (
-    <div className={styles.line}>
-      <div className={styles.lineCategory}>
-        {name}
+    <>
+      <div className={styles.line} >
+        <div className={cn(styles.lineCategory, styles.info)}>
+          <div className={cn(styles.collapseButton, { [styles.collapsed]: isCollapsed })} onClick={collapseCollection}>
+            <ArrowIcon />
+          </div>
+          {name}
+        </div>
+        <div className={cn(styles.lineCategory, styles.actions)}>
+          <div className={styles.plus} onClick={() => { }}>
+            <PlusIcon />
+          </div>
+          <div className={styles.edit} onClick={() => { }}>
+            <EditIcon />
+          </div>
+          <div className={styles.delete} onClick={deleteHandler}>
+            <DeleteIcon />
+          </div>
+        </div>
       </div>
-      <div className={cn(styles.lineCategory, styles.actions)}>
-        <div className={styles.plus} onClick={() => { }}>
-          <PlusIcon />
+      <div className={cn(styles.collectionItems, { [styles.collapsed]: !isCollapsed })}>
+        <div className={styles.collectionItemsInner}>
+          <CollectionItems id={_id} />
         </div>
-        <div className={styles.edit} onClick={() => { }}>
-          <EditIcon />
-        </div>
-        <div className={styles.delete} onClick={deleteHandler}>
-          <DeleteIcon />
-        </div>
-      </div>
-    </div>
+      </div >
+    </>
   )
 }
---
+++ src/widgets/collection-items/ui/index.tsx
@@ -0,0 +1,9 @@
+interface Props {
+  id: string
+}
+
+export const CollectionItems = ({ id }: Props) => {
+  return (
+    <div>{id}</div>
+  )
+}
